4c300a3c0900b4b0daa36667a8a0d6d4d94e42a0
Neko-Life <nekolife123579@gmail.com>
2022-03-30 08:20:01 +0700

    Refactor + bug fixes

--- panint.js
+++ panint.js
@@ -20,50 +20,71 @@ bot.then(async browser => {
     await page.setViewport({ width: 1366, height: 768 });
     await page.goto(baseURI);
     await page.waitForTimeout(5000);
-    const folders = await page.evaluate(() => {
-        const gids = document.getElementsByClassName("item folder");
-        const l = [];
-        for (const v of gids) {
-            l.push(v.lastElementChild.href);
-        }
-        return l;
-    });
-    const download = async (url) => {
+    const folders = await getFolders(page);
+    const download = async (url, level) => {
         const parsedUrl = url.split(/\/+/);
-        const dir = parsedUrl[parsedUrl.length - 2].replace("%20", " ");
-        const save = parsedUrl[parsedUrl.length - 1].replace("%20", " ");
-        const saveDir = baseDir + dir;
+        const dir = [];
+        const save = parsedUrl.pop().replace("%20", " ");
+        for (let i = level + 1; i > 0; i--) {
+            dir.push(parsedUrl[parsedUrl.length - i].replace("%20", " "));
+        }
+        const saveDir = baseDir + dir.join("/");
         let downloaded;
         try {
             downloaded = readdirSync(saveDir);
         } catch (e) {
             downloaded = [];
-            mkdirSync(saveDir);
+            mkdirSync(saveDir, { recursive: true });
         }
         if (downloaded.includes(save)) return;
         const get = await axios.get(url, { responseType: "stream" });
         get.data.pipe(createWriteStream(saveDir + "/" + save));
     }
-    const navFile = async (url) => {
+    async function navFile(url, level) {
         const nPage = await browser.newPage();
         await nPage.setUserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.0 Safari/537.36");
         await nPage.setViewport({ width: 1366, height: 768 });
         await nPage.goto(url);
         await nPage.waitForTimeout(5000);
-        const fileURIS = await nPage.evaluate(() => {
-            const gids = document.getElementsByClassName("item file");
-            const l = [];
-            for (const v of gids) {
-                l.push(v.lastElementChild.href);
-            }
-            return l;
-        });
+        const mFolders = await getFolders(nPage);
+        const fileURIS = await getFiles(nPage);
+        for (const folURL of mFolders) {
+            if (!folURL) continue;
+            if (!folURL.startsWith(url) || folURL === url) continue;
+            await navFile(folURL, level + 1);
+        }
         for (const fileURL of fileURIS) {
-            await download(fileURL);
+            if (!fileURL) continue;
+            await download(fileURL, level);
         }
         await nPage.close();
     }
     for (const fURL of folders) {
-        await navFile(fURL);
+        if (!fURL) continue;
+        await navFile(fURL, 0);
     }
 });
+
+async function getFolders(page) {
+    return evalPage(page, "folder");
+}
+
+async function getFiles(page) {
+    return evalPage(page, "file");
+}
+
+/**
+ * @param {pup.Page} page
+ * @param {"file"|"folder"} type 
+ * @returns 
+ */
+async function evalPage(page, type) {
+    return page.evaluate((type) => {
+        const gids = document.getElementsByClassName("item " + type);
+        const l = [];
+        for (const v of gids) {
+            l.push(v.lastElementChild.href);
+        }
+        return l;
+    }, [type]);
+}
